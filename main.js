de641a18ab23ef6257f979fc5dbd9cff61deb9dd
Amdadur <amdad@live.co.uk>
2021-05-23 08:38:33 +0100

    removal of pages and unused code. api working

--- main.js
+++ main.js
@@ -1,53 +1,20 @@
-// Bing api code below
-//   "https://bing-news-search1.p.rapidapi.com/news/trendingtopics?safeSearch=Off&textFormat=Raw",
-//   {
-//     method: "GET",
-//     headers: {
-//       "x-bingapis-sdk": "true",
-//       "x-rapidapi-key": "15c61ebcbcmsh8c28343ec8cd9d1p12988cjsnc817b17ddb6f",
-//       "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
-//     },
-//   }
-// );
-
-// MISC CODE
-// async function getNews() {
-//   const response = await fetch(
-//     "https://newsapi.org/v2/top-headlines?country=gb&apiKey=0b2791e0cf794881984bd52bce048ce5"
-//   );
-//   const articles = await response.json();
-//   console.log(articles);
-//   return articles;
-// }
-// getNews();
-
 // Start by fetching News API
 // connect to DOM
 
-const list_html = document.getElementById("list");
 const buttonClicker = document.getElementById("click-here");
 const textGenerator = document.getElementById("text");
 const dateSelector = document.querySelector("#date-selector");
+const list_html = document.getElementById("list");
+
 var date = 0;
 
 dateSelector.addEventListener("change", updateValue);
 
 function updateValue(e) {
+    list_html.innerHTML = "";
     date = e.target.value;
     getNews();
 }
-
-//const result = document.querySelector('.result');
-//  result.textContent = `You like ${event.target.value}`;
-//});
-
-//function giveResult() {
-//  document.getElementById("news").innerHTML = "hello"; // the api output;
-//}
-
-//console.log(dateSelector.value);
-
-//buttonClicker.addEventListener("click", getNews);
 
 async function getNews() {
     const response = await fetch(
@@ -55,26 +22,51 @@ async function getNews() {
             date +
             "&to-date=" +
             date +
-            "&api-key=778db1c3-eabf-4572-8025-649d08dd934c"
+            "&api-key=778db1c3-eabf-4572-8025-649d08dd934c&show-fields=body,thumbnail"
     );
     let article = await response.json();
-    //console.log(article);
-    //console.log(article.response.results[1].webTitle);
-    //textGenerator.innerText = article.response.results[1].webTitle;
-    //textGenerator.innerText = article.response.results[2].webTitle;
-    //textGenerator.innerText = article.response.results[3].webTitle;
-    //console.log(textGenerator);
+    console.log(article);
 
     for (var i = 0; i < article.response.results.length; i++) {
+        
         var listItem = document.createElement("li");
-
-        listItem.innerHTML += article.response.results[i].webTitle;
+        listItem.innerHTML +=
+            article.response.results[i].sectionName +
+            "<br>" +
+            article.response.results[i].webTitle +
+            "<br>" +
+            article.response.results[i].webUrl;
         list_html.appendChild(listItem);
+
+
+        var listImg = document.createElement("img");
+        listImg.src += article.response.results[i].fields.thumbnail;
+        list_html.appendChild(listImg);
+
+
+
+        var listBody = document.createElement("div");
+        listBody.innerHTML += article.response.results[i].fields.body;
+        list_html.appendChild(listBody);
+
     }
-    // .then((response) => {
-    //   console.log(response);
-    // })
-    // .catch((err) => {
-    //   console.error(err);
-    // });
 }
+
+//function ShowAndHide() { 
+//    if (listBody.style == "display: none") {
+//        listBody.style = "display = block";
+//    } else {
+//        listBody.style.display = '';
+//    }
+//}
+//        var button = document.createElement('button');
+//button.appendChild(document.createTextNode('delete'));
+//list_html.appendChild(button);
+//buttonFunc = addEventListener("click", ShowAndHide)
+//
+//var showMeBtn = document.createElement("button");
+//showMeBtn.id = "btnFunc";
+//showMeBtn.innerText += "Show/Hide Article";
+//list_html.appendChild(showMeBtn);
+//
+//buttonFunc = addEventListener("click", ShowAndHide);
